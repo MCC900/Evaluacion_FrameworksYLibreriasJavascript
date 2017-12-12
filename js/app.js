865fd443bbe9bc41193c7750a4eb853c400655a0
J. Mariano Cis <mcc900@adinet.com.uy>
2017-12-11 21:01:01 -0300

    Detección y desaparición de lineas de 3 finalizada

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+//Función de easing personalizada para la desaparición de los dulces (soy un mago)
+//Info obtenida de http://brianwald.com/journal/creating-custom-jquery-easing-animations
+$.easing.titilar = function(x, t, b, c, d){
+  var tiempo01 = t / d;
+  var repeticiones = 2; //Modificar para cambiar número de veces que titila
+  var coso = Math.abs(Math.sin(tiempo01 * Math.PI * (repeticiones + 0.5)));
+  return (c - b) * coso + b;
+};
+
+
 //-------------------[ FUNCIONES DE UTILIDAD ]-------------------------
 function intRandom(minimo, maximo){
   return minimo + Math.floor(Math.random() * (1 + maximo - minimo));
@@ -7,9 +17,14 @@ function intRandom(minimo, maximo){
 //Algunas variables de ajuste
 var milisParpadeoTitulo = 600;
 var maxDulcesPorColumna = 7;
-var tiempoCaida = 1000;
-var esperaSpawnDulce = 300;
+var tiempoCaida = 800;
+var esperaSpawnDulce = 200;
+var esperaDesaparicionDulce = 500;
 var milisFadeInOpacidad = 300;
+
+//Datos del Juego
+var estaColumnaRellena = [false, false, false, false, false, false, false]; //Vector de 7 bools que indican si cada columna esta completa
+
 //--------------------[ ANIMACIÓN DEL TÍTULO ]-------------------------
 
 
@@ -75,15 +90,139 @@ function rellenarColumna(numeroColumna){
     if(cantDulces < maxDulcesPorColumna){
       anadirDulceAColumna(numeroColumna);
       window.setTimeout(function(){ rellenarColumna(numeroColumna); }, esperaSpawnDulce);
+    } else {
+      estaColumnaRellena[numeroColumna - 1] = true;
+      if(todasLasColumnasRellenas()){
+        window.setTimeout(verificarDulcesEnLinea, tiempoCaida);
+      }
     }
 }
 
-function llenarTablero(){
+function rellenarTablero(){
   for(var i = 1; i <= 7; i++){
     rellenarColumna(i);
   }
 }
 
+function todasLasColumnasRellenas(){
+  var todasRellenas = true;
+  for(var i = 0; i < 7; i++){
+    todasRellenas &= estaColumnaRellena[i];
+  }
+  return todasRellenas;
+}
+
+function verificarDulcesEnLinea(){
+  var dulcesAEliminar = [];
+
+  for(var x = 0; x < 7; x++){
+    var columna = $(".col-" + (x+1));
+    //Recordar que y es cero para los dulces superiores y va aumentando segun bajamos
+    for(var y = 0; y < maxDulcesPorColumna; y++){
+     //:nth-child empieza en índice 1 según https://api.jquery.com/nth-child-selector/
+      var dulce = columna.find("img:nth-child(" + (y+1) +")");
+      var tipo = dulce.attr('src');
+
+      var arribaIgual = false;
+      var dosArribaIgual = false;
+      var abajoIgual = false;
+      var dosAbajoIgual = false;
+
+      if(y > 0) { arribaIgual = dulceXYesDelTipo(x, y-1, tipo); }
+      if(y > 1)  { dosArribaIgual = dulceXYesDelTipo(x, y-2, tipo); }
+      if(y < maxDulcesPorColumna - 1) { abajoIgual = dulceXYesDelTipo(x, y+1, tipo); }
+      if(y < maxDulcesPorColumna - 2) { dosAbajoIgual = dulceXYesDelTipo(x, y+2, tipo); }
+
+      if(arribaIgual && dosArribaIgual ||
+         arribaIgual && abajoIgual ||
+         abajoIgual && dosAbajoIgual){
+         //Al menos tres en línea vertical
+         dulcesAEliminar[dulcesAEliminar.length] = {elDulce:dulce, posX:x, posY: y};
+         continue;
+      }
+
+      var izquierdaIgual = false;
+      var dosIzquierdaIgual = false;
+      var derechaIgual = false;
+      var dosDerechaIgual = false;
+
+      if(x > 0){ izquierdaIgual = dulceXYesDelTipo(x - 1, y, tipo)}
+      if(x > 1){ dosIzquierdaIgual = dulceXYesDelTipo(x - 2, y, tipo)}
+      if(x < 7){ derechaIgual = dulceXYesDelTipo(x + 1, y, tipo)}
+      if(x < 7){ dosDerechaIgual = dulceXYesDelTipo(x + 2, y, tipo)}
+
+      if(izquierdaIgual && dosIzquierdaIgual ||
+         izquierdaIgual && derechaIgual ||
+         derechaIgual && dosDerechaIgual){
+         //Al menos tres en línea horizontal
+         dulcesAEliminar[dulcesAEliminar.length] = {elDulce:dulce, posX:x, posY: y};
+         continue;
+      }
+    }
+  }
+
+  if(dulcesAEliminar.length > 0){
+    //Comenzamos la animación de eliminación
+    for(var i = 0; i < dulcesAEliminar.length; i++){
+      dulcesAEliminar[i].elDulce.animate({
+        "opacity":"0"
+      },{
+        duration:esperaDesaparicionDulce,
+        queue:false,
+        easing:"titilar"
+      });
+    }
+    window.setTimeout(function(){
+      eliminarDulces(dulcesAEliminar);
+    }, esperaDesaparicionDulce);
+  } else {
+    //No quedan dulces para eliminar. Cedemos el control al usuario
+    cederControlAUsuario();
+  }
+
+  //window.setTimeout(eliminarDulces(dulcesAEliminar), esperaDesaparicionDulce);
+}
+
+function eliminarDulces(dulcesAEliminar){
+
+  var bufferDulcesSinSoporte = [];
+  for(var i = 0; i < dulcesAEliminar.length; i++){
+    var data = dulcesAEliminar[i];
+    //Ajustamos el valor "top" de los dulces que están sobre el que vamos a
+    //eliminar para que no se teletransporten abruptamente a causa del hueco
+    var columna = $(".col-" + (data.posX+1));
+    for(var y = 0; y < data.posY; y++){
+      var dulceArriba = columna.find("img:nth-child(" + (y+1) +")");
+      dulceArriba.css("top", "-=" + dulceArriba.height() + "px");
+      bufferDulcesSinSoporte[bufferDulcesSinSoporte.length] = dulceArriba;
+    }
+  }
+  //Ahora comenzamos las animaciones para que estos caigan
+  for(var i = 0; i < bufferDulcesSinSoporte.length; i++){
+    bufferDulcesSinSoporte[i].animate({
+      "top":"0px"
+    }, tiempoCaida, "swing");
+  }
+
+  //Eliminamos los dulces, y marcamos las columnas para ser rellenadas
+  for(var i = 0; i < dulcesAEliminar.length; i++){
+    dulcesAEliminar[i].elDulce.remove();
+    estaColumnaRellena[dulcesAEliminar[i].posX] = false;
+  }
+
+  //Rellenamos todo
+  rellenarTablero();
+}
+
+function dulceXYesDelTipo(x, y, tipo){
+  var columna = $(".col-" + (x+1));
+  var dulce = columna.find("img:nth-child(" + (y+1) +")");
+  return dulce.attr('src') == tipo;
+}
+
+function cederControlAUsuario(){
+  //TO DO
+}
 //----------------------------[/ JUEGO ]-------------------------------
 
 
@@ -95,6 +234,6 @@ $(function(){
   window.setTimeout(tituloABlanco, milisParpadeoTitulo);
   //maxDulcesPorColumna = Math.floor( $(".col-1").height() / $(".col-1").width());
 
-  llenarTablero();
+  rellenarTablero();
 });
 //-----------------------[/ INICIALIZACIÓN  ]--------------------------
